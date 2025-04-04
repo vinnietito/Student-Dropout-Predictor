4b0ec0246c897acddd6fed406ddaead2bdb414b7
vinnietito <vincentnjuguna69@gmail.com>
2025-04-04 11:55:43 +0300

    Commits

---
+++ student_dropout_predictor.py
@@ -0,0 +1,9 @@
+# student_dropout_predictor
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprecocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, accuracy_score
+
+
